fc4e8224de5e296f3ab8f012f62fed83d59f6484
Karin Thorne <kethorne17@gmail.com>
2019-08-07 18:04:04 -0400

    add react router and rect router dom

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,22 @@
 import React from "react";
 import Header from "./Header";
 import Home from "./Home";
+import { HashRouter as Router } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      {/* <Store>
-        <Router> */}
-      <Header />
-      <div className="photo-bg">
-        <Home />
-        {/* <Route exact path="/" component={Home} />
+      {/* <Store> */}
+      <Router>
+        <Header />
+        <div className="photo-bg">
+          <Home />
+          {/* <Route exact path="/" component={Home} />
         <Route path="/inventory" component={Inventory} />
         <Route path="/product" component={Product} /> */}
-      </div>
-      {/* </Router>
-      </Store> */}
+        </div>
+      </Router>
+      {/* </Store> */}
     </div>
   );
 }
